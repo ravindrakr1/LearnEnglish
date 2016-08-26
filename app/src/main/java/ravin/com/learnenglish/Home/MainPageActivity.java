8c5026581bf7665b89c4745ef347018931aed0d1
Mayank <mgaur_019@hotmail.com>
2016-08-26 13:44:00 +0530

    1. Added Model class for grid view info

--- app/src/main/java/ravin/com/learnenglish/Home/MainPageActivity.java
+++ app/src/main/java/ravin/com/learnenglish/Home/MainPageActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
+
+import java.util.ArrayList;
+
 import ravin.com.learnenglish.R;
 
 
@@ -23,8 +26,19 @@ public class MainPageActivity extends AppCompatActivity {
         this.gridView =(GridView) findViewById(R.id.grid_View);
         this.gridView.setVerticalSpacing(5);
         this.gridView.setHorizontalSpacing(5);
-        String [] NameList = {"Conversation","Quiz","Dictionary","Grammar","Discussion Forum","Daily Phrases","Pronunciation"}; // title Array
-        String[]  ImagesList = {"\uf001","\uf002","\uf003","\uf004","\uf005","\uf006","\uf007"}; // BackGround image array
-        this.gridView.setAdapter(new ravin.com.learnenglish.Home.CustomHomePageAdapter(this, NameList, ImagesList));
+        this.gridView.setAdapter(new ravin.com.learnenglish.Home.CustomHomePageAdapter(this, getGridViewInfo()));
+    }
+
+    protected ArrayList<GridViewInfo> getGridViewInfo(){
+        ArrayList<GridViewInfo> gridInfo = new ArrayList<GridViewInfo>();
+        final String [] nameList = {"Conversation","Quiz","Dictionary","Grammar","Discussion Forum","Daily Phrases","Pronunciation"}; // title Array
+        final String[]  imagesList = {"\uf001","\uf002","\uf003","\uf004","\uf005","\uf006","\uf007"}; // BackGround image array
+        final Integer gridCount = nameList.length;
+        for (Integer i = 0; i < gridCount; ++i ){
+            GridViewInfo grid = new GridViewInfo(nameList[i],imagesList[i]);
+            gridInfo.add(i,grid);
+
+        }
+        return gridInfo;
     }
 }
